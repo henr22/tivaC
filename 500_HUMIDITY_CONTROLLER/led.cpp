4e2265acad777d15d1c932df342ba744d388b968
henr22 <jhfpimentel@gmail.com>
2019-08-31 22:19:49 +0100

    resolved unresolved symbols error

---
+++ 500_HUMIDITY_CONTROLLER/led.cpp
@@ -0,0 +1,30 @@
+/*
+ * led.c
+ *
+ *  Created on: Aug 31, 2019
+ *      Author: jhfpi
+ */
+
+#include <stdint.h>
+#include "inc/tm4c1230h6pm.h"
+#include "led.h"
+
+//Configure on-board led
+/*
+ * Function Name: configureLED
+ * Inputs:
+ * color: It is used to choose wich led is expected to be set. Can have values RED, GREEN, BLUE.
+ */
+void configureLED(uint32_t ledColor)
+{
+    SYSCTL_RCGCGPIO_R   |= (1 << 5); //clock to Port F
+    while(SYSCTL_RCGCGPIO_R & (1 << 5) == 0xFFFFFFFF & (1 << 5)); //It is always needed a 3 clock ticks to set the clock to the GPIO
+    GPIO_PORTF_DIR_R    = (1 << ledColor);
+    GPIO_PORTF_AFSEL_R  &= ~(1 << ledColor);
+    GPIO_PORTF_DEN_R    = (1 << ledColor); //digital enable
+    // For debug
+    GPIO_PORTF_DATA_R   |= (1 << ledColor); //turn on LED
+}
+
+
+
---
+++ 500_HUMIDITY_CONTROLLER/led.h
@@ -0,0 +1,17 @@
+/*
+ * led.h
+ *
+ *  Created on: Aug 31, 2019
+ *      Author: jhfpi
+ */
+
+#ifndef LED_H_
+#define LED_H_
+
+#define RED 1
+#define BLUE 2
+#define GREEN 3
+
+extern void configureLED(uint32_t ledColor);
+
+#endif /* LED_H_ */
